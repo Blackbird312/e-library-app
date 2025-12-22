2e77104b21602d9507595f95b9092b68321eb238
Blackbird312 <elmimouni.med@gmail.com>
2025-12-22 20:06:37 +0100

    [update] Home - loans list

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+import { Loan } from '../../models/loan.model';
+import { LoanService } from 'src/app/services/loans.service';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
   templateUrl: './home.page.html',
-  imports: [CommonModule, IonContent],
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [
+    IonicModule,
+    CommonModule,
+    DatePipe, // enables |date pipe in template
+  ],
 })
-export class HomePage {}
+export class HomePage implements OnInit {
+
+  loans: Loan[] = [];
+  loading = true;
+  error?: string;
+
+  constructor(private loanService: LoanService) {}
+
+  ngOnInit() {
+    this.loadLoans();
+  }
+
+  loadLoans(event?: any) {
+    this.loading = true;
+
+    this.loanService.getLoans().subscribe({
+      next: (data) => {
+        this.loans = data;
+        this.loading = false;
+        event?.target.complete();
+      },
+      error: () => {
+        this.error = 'Failed to load loans';
+        this.loading = false;
+        event?.target.complete();
+      }
+    });
+  }
+}
---
+++ src/app/services/loans.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Loan } from '../models/loan.model';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoanService {
+  private readonly apiUrl = `${environment.baseUrl}/loans`;
+
+  constructor(private http: HttpClient) {}
+
+  getLoans(): Observable<Loan[]> {
+    return this.http.get<Loan[]>(this.apiUrl);
+  }
+}
