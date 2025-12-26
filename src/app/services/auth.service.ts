2c9f6da77683e46cb75ef606438bc613e3060eb3
Blackbird312 <elmimouni.med@gmail.com>
2025-12-26 13:07:55 +0100

    [update] Auth - logout functionality

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
 
   private API_URL = `${environment.baseUrl}/api/auth`;;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient, 
+    private router: Router
+  ) { }
 
   login(username: string, password: string) {
     return this.http.post<any>(`${this.API_URL}/login`, {
@@ -23,6 +27,9 @@ export class AuthService {
 
   logout() {
     localStorage.removeItem('token');
+    this.router.navigateByUrl('/login').then(() => {
+      window.location.reload();
+    });
   }
 
   get token(): string | null {
